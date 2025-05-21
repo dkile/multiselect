912990e76b2ebb339fc64dfb067626455a84cb65
yong <yong@thecodit.com>
2025-05-21 13:51:42 +0900

    env: Fix package.json build output file, type mismatch issue

--- packages/react-multiselect/src/index.ts
+++ packages/react-multiselect/src/index.ts
@@ -1,19 +1,15 @@
-// Public API entrypoint
-export {
-  useMultiSelect,
-  CollapsePositionFirst,
-  CollapsePositionLast,
-} from "./use-multiselect";
-export type { PositionStrategy } from "./use-multiselect";
+export { useMultiSelect } from "./use-multiselect";
 export {
   createMultiSelect,
   type MultiSelectOptions,
   type MultiSelect,
   type MultiSelectState,
-} from "multiselect-core";
-export {
+  UnifiedGroupNode,
+  CollapseManager,
   collapseSelection,
   type CollapsedNode,
   type CollapseOptions,
+  type PositionStrategy,
+  CollapsePositionFirst,
+  CollapsePositionLast,
 } from "multiselect-core";
-export { CollapseManager } from "multiselect-core";
